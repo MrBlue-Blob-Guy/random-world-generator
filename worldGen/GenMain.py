45af13621ea71cc6bc0441ea6522423cfcb75991
MrBlue <debojyotiganguly2006@gmail.com>
2021-07-08 20:28:07 +0530

    updated texture system
    Update GenMain.py
    Added new texture registering system making it easier to add more textures

--- worldGen/GenMain.py
+++ worldGen/GenMain.py
@@ -1,4 +1,6 @@
+import sys
 from ursina import *
+
 
 boxes = []
 #draw blank blocks with some data
@@ -9,14 +11,13 @@ def createBoxes(_position):
 #generate a Place holder world
 def genWorldUntextured():
     for y in range (-5,5):
-        for x in range (1,10):
+        for x in range (-10,10):
             createBoxes((x/2,y/2))
 
 
 def PaintWorld():
-    pass
+    print('repainting World')
 app = Ursina()
-
+#generate untextured world
 genWorldUntextured()
-
 app.run()
